1faefadc0241f81465c0ea21d452d12de1b6bf10
phamxuanminhquan12 <phamxuanminhquan12@gmail.com>
2025-05-12 11:06:17 +0700

    [add] lab-01 ex01 ex02

---
+++ lab-01/ex-02/ex02_01.py
@@ -0,0 +1,5 @@
+# Nhập tên và tuổi từ ngườI dùng
+ten = input ("Nhập tên của bạn: ")
+tuoi = input ("Nhap tuổi của bạn: ")
+# In thông điệp chào mừng vớI thông tin vừa đăng nhập
+print ("Chào mừng,", ten, "! bạn", tuoi,"tuổi.")
